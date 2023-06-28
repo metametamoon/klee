31b93c5e19a9975cd774ece46817f889f6813bd4
Lana243 <lana0771711@gmail.com>
2023-06-28 15:07:47 +0300

    Fix test

--- test/Replay/libkleeruntest/replay_mocks.c
+++ test/Replay/libkleeruntest/replay_mocks.c
@@ -1,7 +1,9 @@
 // RUN: %clang %s -emit-llvm -g %O0opt -c -o %t.bc
 // RUN: rm -rf %t.klee-out
 // RUN: %klee --output-dir=%t.klee-out --external-calls=all --mock-strategy=naive %t.bc
-// RUN: %runmocks %libkleeruntest -o %t.klee-out/a.out %t.klee-out %t.bc
+// RUN: %clang -c %t.bc -o %t.o
+// RUN: %llvmobjcopy --redefine-syms %t.klee-out/redefinitions.txt %t.o
+// RUN: %clang -o %t.klee-out/a.out %libkleeruntest %t.klee-out/externals.ll %t.o
 // RUN: test -f %t.klee-out/test000001.ktest
 // RUN: env KTEST_FILE=%t.klee-out/test000001.ktest %t.klee-out/a.out
 
