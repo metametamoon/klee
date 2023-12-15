2dc12d38a1c70a38b7c577067dd93cc3f9929aff
Maria <mashak1407@gmail.com>
2023-12-15 20:39:05 +0300

    Add TaintSink

--- include/klee/Module/Annotation.h
+++ include/klee/Module/Annotation.h
@@ -38,7 +38,8 @@ enum class Kind {
   Free,
 
   TaintOutput,
-  TaintPropagation
+  TaintPropagation,
+  TaintSink
 };
 
 enum class Property {
@@ -121,6 +122,8 @@ public:
   [[nodiscard]] Kind getKind() const override;
 };
 
+//TODO: maybe separate class Taint
+
 struct TaintOutput final : public Unknown {
   std::string type;
 
@@ -134,6 +137,14 @@ struct TaintPropagation final : public Unknown {
   size_t propagationParameter;
 
   explicit TaintPropagation(const std::string &str = "TaintPropagation");
+
+  [[nodiscard]] Kind getKind() const override;
+};
+
+struct TaintSink final : public Unknown {
+  std::string type;
+
+  explicit TaintSink(const std::string &str = "TaintSink");
 
   [[nodiscard]] Kind getKind() const override;
 };
