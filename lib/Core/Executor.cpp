6d16a25bde555347f69b4d3b446641c3e502e86a
Aleksei Babushkin <ocelaiwo@gmail.com>
2024-01-08 01:38:11 +0500

    [save] multiplex and mock

--- lib/Core/Executor.cpp
+++ lib/Core/Executor.cpp
@@ -361,6 +361,10 @@ cl::opt<bool> MockAllExternals("mock-all-externals", cl::init(false),
                                cl::desc("If true, all externals are mocked."),
                                cl::cat(ExtCallsCat));
 
+cl::opt<bool> MockAllFunctions("tmp-mock-all", cl::init(false),
+                               cl::desc("If true, all externals are mocked."),
+                               cl::cat(ExtCallsCat));
+
 /*** Seeding options ***/
 
 cl::opt<bool> AlwaysOutputSeeds(
@@ -2009,6 +2013,7 @@ ref<klee::ConstantExpr> Executor::getEhTypeidFor(ref<Expr> type_info) {
 
 void Executor::executeCall(ExecutionState &state, KInstruction *ki, Function *f,
                            std::vector<ref<Expr>> &arguments) {
+
   Instruction *i = ki->inst();
   if (isa_and_nonnull<DbgInfoIntrinsic>(i))
     return;
@@ -2331,6 +2336,13 @@ void Executor::executeCall(ExecutionState &state, KInstruction *ki, Function *f,
     // KInstIterator from just an instruction (unlike LLVM).
     KFunction *kf = kmodule->functionMap[f];
 
+    if (MockAllFunctions && state.multiplexKF) {
+      if (!kf->getFunctionType()->getReturnType()->isVoidTy()) {
+        prepareMockValue(state, "mock_return_sa", ki);
+      }
+      return;
+    }
+
     state.pushFrame(state.prevPC, kf);
     transferToBasicBlock(&*kf->function()->begin(), state.getPrevPCBlock(),
                          state);
@@ -2465,6 +2477,19 @@ void Executor::executeCall(ExecutionState &state, KInstruction *ki, Function *f,
     for (unsigned k = 0; k < numFormals; k++)
       bindArgument(kf, k, state, arguments[k]);
   }
+
+  if (!state.multiplexKF && !f->isIntrinsic() && !f->isDeclaration() &&
+      kmodule->mainModuleFunctions.count(std::string(f->getName()))) {
+    state.multiplexKF = ki->getKModule()->functionMap.at(f);
+    multiplexReached++;
+    // if (multiplexReached % 20 == 0) {
+    //   llvm::errs() << "Multiplex reached: " << multiplexReached << "\n";
+    // }
+  }
+
+  // if (!state.multiplexKF && !f->isIntrinsic() && !f->isDeclaration() &&
+  //     ki->getKFunction()->getName() == "__klee_multiplexed_entry") {
+  // }
 }
 
 void Executor::increaseProgressVelocity(ExecutionState &state, KBlock *block) {
@@ -4736,6 +4761,8 @@ void Executor::terminateState(ExecutionState &state,
     klee_warning("remove state twice");
     return;
   }
+
+  // llvm::errs() << state.constraints.path().toString() << "\n";
 
   interpreterHandler->incPathsExplored();
   state.pc = state.prevPC;
