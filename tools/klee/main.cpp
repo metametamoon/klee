269655388e9c6dd2aedfecf28d518e6e23bb7c97
Aleksei Babushkin <ocelaiwo@gmail.com>
2024-01-12 18:39:42 +0500

    [fix] SAMultiplexKind

--- tools/klee/main.cpp
+++ tools/klee/main.cpp
@@ -409,6 +409,19 @@ cl::opt<bool> AnnotateOnlyExternal(
 cl::opt<bool> multiplexForStaticAnalysis("multiplex-static-analysis",
                                          llvm::cl::desc(""),
                                          llvm::cl::cat(StartCat));
+
+enum class SAMultiplexKind {
+  None,
+  Module,
+  All,
+};
+
+cl::opt<SAMultiplexKind> multiplexForStaticAnalysis(
+    "multiplex-static-analysis",
+    cl::values(clEnumValN(SAMultiplexKind::None, "none", ""),
+               clEnumValN(SAMultiplexKind::Module, "module", ""),
+               clEnumValN(SAMultiplexKind::All, "all", "")),
+    cl::desc(""), cl::init(SAMultiplexKind::None), cl::cat(StartCat));
 } // namespace
 
 namespace klee {
@@ -2032,24 +2045,35 @@ int main(int argc, char **argv, char **envp) {
     mainModuleGlobals.insert(gv.getName().str());
   }
 
-  if (multiplexForStaticAnalysis) {
+  if (multiplexForStaticAnalysis != SAMultiplexKind::None) {
     assert(UseGuidedSearch != Interpreter::GuidanceKind::ErrorGuidance);
 
     std::vector<llvm::Function *> Fns;
 
-    for (auto &f : *mainModule) {
-      if (!f.isDeclaration()) {
-        Fns.push_back(&f);
+    switch (multiplexForStaticAnalysis) {
+    case SAMultiplexKind::All: {
+      for (auto &f : *mainModule) {
+        if (!f.isDeclaration()) {
+          Fns.push_back(&f);
+        }
       }
+      break;
     }
 
-    // for (const auto &mod : functionsByModule.modules) {
-    //   for (const auto &f : mod) {
-    //     if (functionsByModule.usesInModule[f] <= 1) {
-    //       Fns.push_back(f);
-    //     }
-    //   }
-    // }
+    case SAMultiplexKind::Module: {
+      for (const auto &mod : functionsByModule.modules) {
+        for (const auto &f : mod) {
+          if (functionsByModule.usesInModule[f] <= 1) {
+            Fns.push_back(f);
+          }
+        }
+      }
+      break;
+    }
+
+    default:
+      assert(0);
+    }
 
     multiplexEntryPoint(mainModule, ctx, Fns);
   }
