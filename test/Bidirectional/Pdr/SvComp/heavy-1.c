214aebb1b522718ac90ad7e35522bb0ca866ac1a
metametamoon <sorvinlevs@gmail.com>
2025-05-22 22:08:13 +0300

    fix: better test structure

---
+++ test/Bidirectional/Pdr/SvComp/heavy-1.c
@@ -0,0 +1,30 @@
+// REQUIRES: geq-llvm-12.0
+
+// RUN: rm -rf %t.klee-out || echo "ok"
+// RUN: %kleef --bidirectional --backwards-full-logs --output-dir=%t.klee-out --property-file=%S/unreach-call.prp --max-memory=15000000000 --max-cputime-soft=900 --32 %s &>%t.log
+// RUN: FileCheck %s -input-file=%t.log
+// CHECK: [TRUE POSITIVE]
+
+extern void abort(void);
+extern void __assert_fail(const char *, const char *, unsigned int, const char *) __attribute__ ((__nothrow__ , __leaf__)) __attribute__ ((__noreturn__));
+void reach_error() { __assert_fail("0", "heavy-1.c", 3, "reach_error"); }
+
+/* Licensed under the GPLv2 */
+
+int main(int argc, char **argv)
+{
+  static char array[1024 * 1024] = { 1 };
+  unsigned int a = 1, i, j, k;
+
+  for (i = 0; i < sizeof(array); i++)
+    for (j = 0; j < sizeof(array); j++)
+      for (k = 0; k < sizeof(array); k++)
+        array[i] = array[j] + array[k];
+
+  if (a != 0)
+    goto ERROR;
+
+  return array[100];
+  ERROR: {reach_error();abort();}
+  return 1;
+}
