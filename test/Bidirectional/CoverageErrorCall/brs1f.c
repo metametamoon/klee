1f40be3078868118007326c1678ee77b914f1191
metametamoon <sorvinlevs@gmail.com>
2025-07-07 09:40:19 +0300

    test: disable not working tests on free-bsd

--- test/Bidirectional/CoverageErrorCall/brs1f.c
+++ test/Bidirectional/CoverageErrorCall/brs1f.c
@@ -1,6 +1,7 @@
 // REQUIRES: geq-llvm-12.0
 // REQUIRES: not-darwin
 // (deterministic memory allocation not supported on darwin)
+// REQUIRES: not-freebsd
 
 // RUN: %clang %s -emit-llvm %O0opt -fno-discard-value-names -g -c -o %t.bc
 // RUN: %clang %testcomp_defs -emit-llvm -g -c -o %t.testcomp_defs.bc
